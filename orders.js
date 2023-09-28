f6b2b7551781b51013d259d3dc973f77b970c03d
badwaikparas <111993734+badwaikparas@users.noreply.github.com>
2023-09-28 20:53:39 +0530

    version1

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Laptop',
+        productNumber: '#123ABC',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Smartphone',
+        productNumber: '#789XYZ',
+        paymentStatus: 'Pending',
+        shipping: 'In Transit'
+    },
+    {
+        productName: 'Headphones',
+        productNumber: '#456LMN',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Tablet',
+        productNumber: '#999PQR',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Camera',
+        productNumber: '#222DEF',
+        paymentStatus: 'Pending',
+        shipping: 'Processing'
+    }
+];
